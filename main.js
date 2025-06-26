3680564e834a9cc4b6876428dacf6dc81ac16b03
Tanecodes <tanecodes@gmail.com>
2025-06-27 01:17:19 +1000

    finished the logic for creating a task and deleting it

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // this  project is to teach me more javascript logic
 // ive created a todo app before but this one is going to be created in Javascript only
 
-// settig some variables
+// setting some variables
 const app = document.getElementById("app");
 const body = document.body;
 
@@ -12,13 +12,13 @@ body.style.display = "flex";
 body.style.justifyContent = "center";
 body.style.alignItems = "center";
 
-// creating the title and its styles
+// creating the title(h1) and setting its styles
 const title = document.createElement("h1");
 
 title.innerText = "TODO APP";
 title.style.color = "white";
 title.style.fontSize = "3rem";
-
+// appending the title to the app.
 app.appendChild(title);
 
 // creating an input field and a button for the user.
@@ -43,22 +43,43 @@ btn.style.display = "flex";
 btn.style.justifyContent = "center";
 btn.style.alignItems = "center";
 
+// appending input and confirm button to the app (div element).
 app.appendChild(input);
 app.appendChild(btn);
 
 // creating an event listener for the button
-// which will grab the input 
+// which will grab the input and create a task.
 
-btn.addEventListener("click", function() {
-  
+btn.addEventListener("click", function() { 
   const userInput = input.value;
 
+  // creating an if statement for the logic.
   if(userInput.trim() !== "") {
-    
-    const task = document.createElement("p");
-    task.innerText = userInput;
-    task.style.color = "white";
+
+    // setting variables to create elements.
+    const task = document.createElement("div");
+    const text = document.createElement("p");
+
+    const deleteBtn = document.createElement("button");
+    // creating an event listener for the delete button.
+    deleteBtn.addEventListener("click", function() {
+      task.remove();
+    });
+
+    // task element styles.
+    task.style.display = "flex";
+    task.style.flexDirection = "row";
+    // paragraph element styles (task text).
+    text.innerText = userInput;
+    text.style.color = "white";
+    // delete button styles.
+    deleteBtn.innerText = "delete";
+
+    // appending the elements created so they appear in the browser.
+    task.appendChild(text);
+    task.appendChild(deleteBtn);
     app.appendChild(task);
+
     input.value = "";
   }
 
